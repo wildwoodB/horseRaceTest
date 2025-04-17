13662c68971faaac1f2fbd3facaab69691cf9a26
wildwoodB <borisov.nee@gmail.com>
2025-04-17 13:52:55 +0700

    Initial Commit

---
+++ horseRaceTest1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  horseRaceTest1
+//
+//  Created by Никита Борисов on 17/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ horseRaceTest1/horseRaceTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  horseRaceTest1App.swift
+//  horseRaceTest1
+//
+//  Created by Никита Борисов on 17/4/25.
+//
+
+import SwiftUI
+
+@main
+struct horseRaceTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
